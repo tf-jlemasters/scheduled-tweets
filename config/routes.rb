03fa8aec62ac211911ad7e4fa91844fcd77ecf3b
tf-jlemasters <jlemasters@tracefirst.com>
2022-03-22 16:41:23 -0600

    added user model, created sign-up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get "/about-us", to: "about#index", as: :about
 
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "regristrations#create"
+
   root to: "main#index"
 end
