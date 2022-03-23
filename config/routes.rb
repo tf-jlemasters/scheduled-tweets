4db6ff0fad1d3af10df5401e64e5b5e8a31a265e
tf-jlemasters <jlemasters@tracefirst.com>
2022-03-23 09:31:03 -0600

    added sign up form, session cookies, and ability to track users either logged in or logged out.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   get "/about-us", to: "about#index", as: :about
 
   get "sign_up", to: "registrations#new"
-  post "sign_up", to: "regristrations#create"
+  post "sign_up", to: "registrations#create"
+
+  get "sign_in", to: "sessions#create"
+  post "sign_in", to: "sessions#create"
+
+  delete "logout", to: "session#destroy"
 
   root to: "main#index"
 end
